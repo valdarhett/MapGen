818d05ab68fe8d550716ed138c96bed11fbe5177
WIN <valdarhett@gmail.com>
2016-02-12 00:54:20 +0800

    First Commit

---
+++ core/src/com/mapgenerator/mapgen/MapGenMain.java
@@ -0,0 +1,55 @@
+package com.mapgenerator.mapgen;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
+public class MapGenMain extends ApplicationAdapter {
+	ShapeRenderer sr;
+	Float[][] noise;
+	int width = 0;
+	int height = 0;
+
+	@Override
+	public void create() {
+		sr = new ShapeRenderer();
+		Perlin perlin = new Perlin();
+
+		width = (int) (Gdx.graphics.getWidth() * 1.2);
+		height = (int) (Gdx.graphics.getHeight() * 1.2);
+		// noise = perlin.getSmoothNoise(width, height, 8);
+		noise = perlin.getPerlin(width, height, 7, 0.1f);
+		noise = perlin.Equalize(noise, width, height);
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		sr.begin(ShapeType.Filled);
+		Float max = 0.0f, min = 1.0f;
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+
+				float buffer = noise[x][y];
+				max = max > buffer ? max : buffer;
+				min = min < buffer ? min : buffer;
+				Color color = new Color(buffer, buffer, buffer, 1.0f);
+				// if (buffer < 0.3) {
+				// color = new Color(1.0f, buffer*2.5f,1.0f, 0.5f);
+				// } else if (buffer < 0.5 && buffer >=0.3) {
+				// color = new Color(1.0f, buffer, 1.0f, 0.5f);
+				// } else {
+				// color = new Color(1.0f, 1.0f, buffer, 0.5f);
+				// }
+
+				sr.rect(x, y, 1.0f, 1.0f, color, color, color, color);
+			}
+		}
+		sr.end();
+		Gdx.app.log("min max", min + " " + max);
+	}
+}
---
+++ core/src/com/mapgenerator/mapgen/Perlin.java
@@ -0,0 +1,126 @@
+package com.mapgenerator.mapgen;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
+
+import com.badlogic.gdx.Gdx;
+
+public class Perlin {
+
+	/**
+	 * normal noise
+	 * 
+	 * @param width
+	 * @param height
+	 * @return float[][] containing noise info
+	 */
+	public Float[][] getWhiteNoise(int width, int height) {
+		Float[][] noise = new Float[width][height];
+		Random random = new Random(System.currentTimeMillis());
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				noise[x][y] = random.nextFloat();
+			}
+		}
+		return noise;
+	}
+
+	public Float[][] getSmoothNoise(int width, int height, int octave) {
+		int samplePeriod = (int) Math.pow(2, octave - 1);
+		Float[][] baseNoise = getWhiteNoise(width, height);
+		float sampleFrequency = 1.0f / samplePeriod;
+		Gdx.app.log("frequency", sampleFrequency + "");
+		Float[][] smoothNoise = new Float[width][height];
+		for (int y = 0; y < height; y++) {
+			int sampley0 = (y / samplePeriod) * samplePeriod;
+			int sampley1 = (sampley0 + samplePeriod) % height;
+			float verticalBlend = (y - sampley0) * sampleFrequency;
+			for (int x = 0; x < width; x++) {
+				int samplex0 = (x / samplePeriod) * samplePeriod;
+				int samplex1 = (samplex0 + samplePeriod) % width;
+				float horizontalBlend = (x - samplex0) * sampleFrequency;
+
+				float top = interpolate(baseNoise[samplex0][sampley0], baseNoise[samplex0][sampley1], verticalBlend);
+				float bottom = interpolate(baseNoise[samplex1][sampley0], baseNoise[samplex1][sampley1], verticalBlend);
+
+				// smoothNoise[x][y] = verticalBlend;
+				smoothNoise[x][y] = interpolate(top, bottom, horizontalBlend);
+			}
+		}
+		return smoothNoise;
+	}
+
+	private float interpolate(float x0, float x1, float t) {
+		return x0 * (1 - t) + t * x1;
+	}
+
+	public Float[][] getPerlin(int width, int height, int octaveCount, float persistance) {
+		float amplitude = 1.0f;
+		float totalAmplitude = 0.0f;
+		Float[][][] noise = new Float[octaveCount][][];
+		Float[][] perlin = new Float[width][height];
+
+		for (Float[] row : perlin) {
+			Arrays.fill(row, 0.0f);
+		}
+
+		for (int i = 1; i < octaveCount; i++) {
+			noise[i] = getSmoothNoise(width, height, i);
+		}
+
+		for (int i = octaveCount - 1; i > 0; i--) {
+			amplitude *= persistance;
+			totalAmplitude += amplitude;
+
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					perlin[x][y] += noise[i][x][y] * amplitude;
+				}
+			}
+		}
+		// normalization
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				perlin[x][y] /= totalAmplitude;
+			}
+		}
+
+		return perlin;
+	}
+
+	/**
+	 * normalizing function
+	 * 
+	 * @param source
+	 * @param width
+	 * @param height
+	 * @return
+	 */
+	public Float[][] Equalize(Float[][] source, int width, int height) {
+		HashMap<Float, Integer> pCount = new HashMap<Float, Integer>();
+		Float[][] buffer = new Float[width][height];
+		Float max = 0.0f, min = 1.0f;
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (!pCount.containsKey(source[x][y])) {
+					pCount.put(source[x][y], 1);
+				} else {
+					pCount.put(source[x][y], pCount.get(source[x][y]) + 1);
+				}
+				min = min < source[x][y] ? min : source[x][y];
+				max = max > source[x][y] ? max : source[x][y];
+			}
+		}
+
+		Float dynamic = max - min;
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				buffer[x][y] = ((source[x][y] - min) / dynamic);
+			}
+		}
+
+		return buffer;
+	}
+}
